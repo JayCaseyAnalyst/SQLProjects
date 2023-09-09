1a2536f8275fdc14a4a6f3b57e199ec56e98be74
Jay Casey <jay.w.casey@gmail.com>
2023-09-09 10:07:08 -0400

    Add files via upload

---
+++ RoadAccidents.sql
@@ -0,0 +1,79 @@
+SELECT
+	*
+FROM
+	[Road Accident]..road_accident
+
+-- Current Year Casualties (2022) For Dry Surface Conditions
+SELECT
+	SUM(number_of_casualties) AS CY_Casualties
+FROM 
+	[Road Accident]..road_accident
+WHERE
+	YEAR(accident_date) = '2022' 
+	--AND road_surface_conditions = 'Dry'
+
+
+-- 2022 Accidents
+SELECT
+	COUNT(DISTINCT accident_index) AS CY_Accidents
+FROM 
+	[Road Accident]..road_accident
+WHERE
+	YEAR(accident_date) = '2022' 
+	--AND road_surface_conditions = 'Dry'
+
+-- 2022 Fatal Casualties
+SELECT
+	SUM(number_of_casualties) AS CY_Fatal_Casualties
+FROM 
+	[Road Accident]..road_accident
+WHERE
+	YEAR(accident_date) = '2022' 
+	AND accident_severity = 'Fatal'
+	--AND road_surface_conditions = 'Dry'
+
+-- 2022 Serious Casualties
+SELECT
+	SUM(number_of_casualties) AS CY_Serious_Casualties
+FROM 
+	[Road Accident]..road_accident
+WHERE
+	YEAR(accident_date) = '2022' 
+	AND accident_severity = 'Serious'
+	--AND road_surface_conditions = 'Dry'
+
+-- 2022 Slight Casualties
+SELECT
+	SUM(number_of_casualties) AS CY_Slight_Casualties
+FROM 
+	[Road Accident]..road_accident
+WHERE
+	YEAR(accident_date) = '2022' 
+	AND accident_severity = 'Slight'
+	--AND road_surface_conditions = 'Dry'
+
+-- 2022 Casualties by Severity
+SELECT
+	accident_severity
+	, SUM(number_of_casualties) AS CY_Casualties
+FROM 
+	[Road Accident]..road_accident
+WHERE 
+	YEAR(accident_date) = '2022'
+GROUP BY
+	accident_severity
+ORDER BY 
+	1
+
+-- 2022 Accidents by Light Conditions
+SELECT 
+	light_conditions
+	, COUNT(DISTINCT accident_index) AS CY_Accidents
+FROM
+	[Road Accident]..road_accident
+WHERE 
+	YEAR(accident_date) = '2022'
+GROUP BY 
+	light_conditions
+ORDER BY 
+	2 DESC
